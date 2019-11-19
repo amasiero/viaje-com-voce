65afbf65cff5d634815f5ed923390b954221b8b9
Andrey Masiero <andreymasiero@gmail.com>
2019-11-18 22:10:53 -0300

    finished shopping cart

---
+++ frontend/src/cart/checkout.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+// import pagarme from 'pagarme'
+
+export default () => {
+  return (
+    <div className='container'>
+      <h2 className='section-subtitle full-size'>Finalizar Pagamento</h2>
+    </div>
+  )
+}
